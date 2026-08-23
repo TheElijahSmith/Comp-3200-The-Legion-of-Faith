8bd7d7f8b30e0b34d75ef6416e1f65e35fb233d6
kcastellanos14 <kcastellanos@harding.edu>
2026-08-23 17:43:23 -0500

    Create kcastellanos14.py

---
+++ transmissions/kcastellanos14.py
@@ -0,0 +1,16 @@
+# transmissions/kcastellanos14.py
+# A short first transmission to the crew.
+
+def main():
+    # Print my name for the crew
+    print("Name: Kevin Castellanos")
+    
+    # Print a fun fact for the team
+    print("Fact: I collect rubik cubes, and love to play soccer.")
+    
+    # Print one problem you'd love deep learning to help solve.
+    print("Problem: I would like to use Deep Learning to create an app that translates sign language to text in real time.")
+
+# Run main() only when this file is executed directly, not when it's imported.
+if __name__ == "__main__":
+    main()
