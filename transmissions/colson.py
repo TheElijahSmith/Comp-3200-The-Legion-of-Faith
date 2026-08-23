da811905f0b904d06d444715b13eb62317a70876
csipe4 <csipe4@harding.edu>
2026-08-23 15:48:22 -0500

    [Week1] Add transmission for Colson Sipe

---
+++ transmissions/colson.py
@@ -0,0 +1,13 @@
+# transmission/colson.py
+
+# this function introduces me when it is called
+def main():
+    # these lines will print my personal info
+    print("Name: Colson Sipe")
+    print("Fact: I can name every Survivor season and winner in order.")
+    print("Problem: I want to use deep learning to help solve space related problems such as rover navigation"
+    ", celestial body identification, or anything else in that field.")
+
+# this will call the main function if the condition is met
+if __name__ == "__main__":
+    main()
