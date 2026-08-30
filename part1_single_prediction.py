3f390529edcd5debbecbce6df75ff9ff82b03a90
kcastellanos14 <kcastellanos@harding.edu>
2026-08-30 18:57:21 -0500

    Update part1_single_prediction.py

--- part1_single_prediction.py
+++ part1_single_prediction.py
@@ -1,15 +1,11 @@
-# List of blade angle values from the sparring sessions
+# Lectures
 blade_angle = [8.5, 9.5, 9.9, 9.0]
-# Starting weight value to control prediction size
 weight = 0.5
 
 def neural_network(input, weight):
-    # Multiply the single input number by the weight number
     return input * weight
-    
-# Loop through each blade angle reading
+
 for angle in blade_angle:
-    # Get prediction for current blade angle
     pred = neural_network(angle, weight)
     print(f"Blade Angle: {angle} | Prediction: {pred}")
 
