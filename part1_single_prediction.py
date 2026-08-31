d1776b0b5125193f8fd4c631036a01d6b85d347e
kcastellanos14 <kcastellanos@harding.edu>
2026-08-30 19:00:32 -0500

    Update part1_single_prediction.py

--- part1_single_prediction.py
+++ part1_single_prediction.py
@@ -1,15 +1,21 @@
-# Lectures
+# List of blade angle values from the sparring sessions
 blade_angle = [8.5, 9.5, 9.9, 9.0]
+# Starting weight value to control prediction size
 weight = 0.5
 
 def neural_network(input, weight):
+    # Multiply the single input number by the weight number
     return input * weight
 
+# Loop through each blade angle reading
 for angle in blade_angle:
+    # Get prediction for current blade angle
     pred = neural_network(angle, weight)
     print(f"Blade Angle: {angle} | Prediction: {pred}")
 
 """
+ANWER TO QUESTIONS
+
 1. What does the weight do?
    Controls how much influence an input feature has on the output prediction.
 
