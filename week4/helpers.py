da3a6600402e56e007f865cb57fb35c6322a1c64
TrooperBrady <bjones39@harding.edu>
2026-09-13 18:04:05 -0500

    Clean up helpers.py by removing commented code
    Removed unused angle balance breath weights and input example.

--- week4/helpers.py
+++ week4/helpers.py
@@ -14,11 +14,3 @@ def vect_mat_mul(vector, matrix):
                           matrix[i])
     return output
 
-# angle balance breath
-weights = [[0.1, 0.1, -0.3], # opens left?
-            [0.1, 0.2, 0.0], # strikes high?
-            [0.0, 1.3, 0.1]] # feints?
-
-input = [8.5, .65, 1.2]
-pred = vect_mat_mul(input, weights)
-print(pred) # [0.555, 0.9800000000000001, 0.9650000000000001]
